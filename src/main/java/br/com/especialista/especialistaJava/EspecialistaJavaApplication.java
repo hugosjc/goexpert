284521ed81659c51569f08de48aaa9daebbedfa2
hugosjc <hugo.alexandro1@gmail.com>
2022-06-23 20:16:53 -0300

    first commit

---
+++ src/main/java/br/com/especialista/especialistaJava/EspecialistaJavaApplication.java
@@ -0,0 +1,13 @@
+package br.com.especialista.especialistaJava;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class EspecialistaJavaApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(EspecialistaJavaApplication.class, args);
+	}
+
+}
---
+++ src/test/java/br/com/especialista/especialistaJava/EspecialistaJavaApplicationTests.java
@@ -0,0 +1,13 @@
+package br.com.especialista.especialistaJava;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class EspecialistaJavaApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
