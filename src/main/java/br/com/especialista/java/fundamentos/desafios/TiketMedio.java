5a11c3f723631af255abfc1ad246a9bdf1676fe1
hugosjc <hugo.alexandro1@gmail.com>
2022-06-23 21:42:52 -0300

    desafio

---
+++ src/main/java/br/com/especialista/java/fundamentos/desafios/TiketMedio.java
@@ -0,0 +1,14 @@
+package br.com.especialista.java.fundamentos.desafios;
+
+public class TiketMedio {
+
+    public static void main(String [] args) {
+
+        int venda1 = 800;
+        int venda2 = 451;
+        int venda3 = 685;
+
+        int media = (venda1 + venda2 + venda3) /3;
+        System.out.print("O ticket médio das vendas foi: " + media);
+    }
+}
